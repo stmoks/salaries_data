77336c810982d42637f4a8f11405dd1e9c994564
Sechaba Mokobane <sechaba.mokobane@gmail.com>
2021-06-28 21:16:17 +0200

    Added the README and main.py file

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 An exploration of salary data across majors. Completed as part of day 71 of Angela Yu's '100 Days of Code' course on Udemy.
 
-The data is sourced
+The data is sourced form PayScale Inc. based on the period between 1998 and 2008.
--- main.py
+++ main.py
@@ -1,6 +1,59 @@
 import pandas as pd
 
-salaries_data = pd.read_csv('salaries_by_college_major.csv')
+raw_data = pd.read_csv('salaries_by_college_major.csv')
+
 
 # data exploration
-print(salaries_data.count())
+# print(f'\nHead:\n {raw_data.head()}')
+# print(f'\nShape:\n {raw_data.shape}')
+
+# print(f'\nColumns:\n {raw_data.columns}')
+
+clean_data = raw_data.dropna()
+
+# print(f'\nTail:\n {clean_data.tail()}')
+
+
+# # Highest starting median salary
+
+# max_salary = clean_data['Starting Median Salary'].max()
+# print(clean_data[clean_data['Starting Median Salary'] == max_salary])
+
+# # or...
+# id_max = clean_data['Starting Median Salary'].idxmax()
+# print(clean_data.loc[id_max])
+
+
+
+# # Highest mid-career salary
+# max_salary = clean_data['Mid-Career Median Salary'].max()
+# print(clean_data[clean_data['Mid-Career Median Salary'] == max_salary])
+
+# # Lowest starting salary
+# min_salary = clean_data['Starting Median Salary'].min()
+# print(clean_data[clean_data['Starting Median Salary'] == min_salary])
+
+
+# Low risk majors
+salary_spread = clean_data['Mid-Career 90th Percentile Salary'] - clean_data['Mid-Career 10th Percentile Salary']
+clean_data.insert(1,'Salary spread',salary_spread)
+low_risk = clean_data['Salary spread'].min()
+print(clean_data[clean_data['Salary spread'] == low_risk])
+
+#or...
+print(clean_data.sort_values('Salary spread').head())
+
+
+
+# Number of majors per group
+print(clean_data.groupby('Group').count())
+
+# Average salary per group
+pd.options.display.float_format = '{:,.2f}'.format 
+print(clean_data.groupby('Group').mean().sort_values(['Starting Median Salary'],ascending=False))
+
+
+
+
+
+
